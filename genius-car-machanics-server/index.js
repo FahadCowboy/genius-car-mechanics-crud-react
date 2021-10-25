049d9990127258f310e201d0d6623a2eb02d4304
FahadCowboy <olivierafahad@gmail.com>
2021-10-26 04:05:56 +0600

    database initially set up

--- genius-car-machanics-server/index.js
+++ genius-car-machanics-server/index.js
@@ -1,8 +1,33 @@
 const {MongoClient} = require('mongodb')
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
+const port = process.env.PORT || 4000
 
 // MiddleWire
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z46cs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+const run = async () => {
+   try{
+      await client.connect();
+      console.log('database ready')
+
+   } finally{
+      await client.close();
+   }
+}
+run().catch(console.dir)
+app.get('/', (req, res) => {
+   console.log(uri)
+   res.send('Project is working well!')
+})
+
+
+app.listen(port, (req, res) => {
+   console.log('This server is running at port no: ', port)
+})
