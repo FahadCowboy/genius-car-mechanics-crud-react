feb1496f81e72979950721880fe7aca8eba4ed65
FahadCowboy <olivierafahad@gmail.com>
2021-10-27 01:29:15 +0600

    delete api for single service is done and project is almost done

--- genius-car-machanics-server/index.js
+++ genius-car-machanics-server/index.js
@@ -1,6 +1,7 @@
-const {MongoClient} = require('mongodb')
+const {MongoClient, ObjectID} = require('mongodb')
 const express = require('express')
 const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 4000
@@ -19,19 +20,35 @@ const run = async () => {
       const database = client.db('carMechanics')
       const servicesCollection = database.collection('services')
 
-      // GET API
+      // GET all services API
       app.get('/services', async (req, res) => {
          const cursor = servicesCollection.find({})
          const services = await cursor.toArray()
          res.send(services)
       })
 
+      // GET specific service API
+      app.get('/services/:id', async (req, res) => {
+         const id = req.params.id
+         console.log(id)
+         const query = {_id: ObjectId(id)}
+         const result = await servicesCollection.findOne(query)
+         console.log(result)
+         res.send(result)
+      })
+
       // POST API
       app.post('/services', async (req, res) => {
          const service = req.body
          const result = await servicesCollection.insertOne(service)
+         res.send(result)
+      })
 
-         // console.log(result)
+      // Delete single service API
+      app.delete('/services/:id', async(req, res) => {
+         const id = req.params.id
+         const query = {_id: ObjectId(id)}
+         const result = await servicesCollection.deleteOne(query)
          res.send(result)
       })
 
